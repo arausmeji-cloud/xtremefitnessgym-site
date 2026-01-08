de4d63173501bca327b1fcbff738ace9d465edec
arausmeji-cloud <arausmeji@gmail.com>
2026-01-08 16:07:17 +1100

    Update gallery-gym.js

--- public/js/gallery-gym.js
+++ public/js/gallery-gym.js
@@ -1,49 +1,54 @@
-let photos = [];
-let currentIndex = 0;
-const folder = "gym";
+document.addEventListener("DOMContentLoaded", () => {
 
-const container = document.getElementById("gym-container");
-const lightbox = document.getElementById("lightbox");
-const lightboxImg = document.getElementById("lightbox-img");
-const prevBtn = document.getElementById("prev-btn");
-const nextBtn = document.getElementById("next-btn");
+  let photos = [];
+  let currentIndex = 0;
+  const folder = "gym";
 
-fetch("/api/list-photos?folder=" + folder)
-  .then(res => res.json())
-  .then(data => {
-    photos = data;
-    photos.forEach((file, index) => {
-      const img = document.createElement("img");
-    img.src = "/images/gallery/" + folder + "/" + file;
-      img.className = "gallery-thumb";
-      img.dataset.index = index;
+  const container = document.getElementById("gym-container");
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+  const prevBtn = document.getElementById("prev-btn");
+  const nextBtn = document.getElementById("next-btn");
 
-      img.addEventListener("click", () => {
-        currentIndex = index;
-        openLightbox();
+  fetch("/api/list-photos?folder=" + folder)
+    .then(res => res.json())
+    .then(data => {
+      photos = data;
+
+      photos.forEach((file, index) => {
+        const img = document.createElement("img");
+        img.src = "/images/gallery/" + folder + "/" + file;
+        img.className = "gallery-thumb";
+        img.dataset.index = index;
+
+        img.addEventListener("click", () => {
+          currentIndex = index;
+          openLightbox();
+        });
+
+        container.appendChild(img);
       });
-
-      container.appendChild(img);
     });
-  });
 
-function openLightbox() {
-  lightbox.style.display = "flex";
-  lightboxImg.src = "/images/gallery/" + folder + "/" + photos[currentIndex];
-}
+  function openLightbox() {
+    lightbox.style.display = "flex";
+    lightboxImg.src = "/images/gallery/" + folder + "/" + photos[currentIndex];
+  }
 
-prevBtn.onclick = e => {
-  e.stopPropagation();
-  currentIndex = (currentIndex - 1 + photos.length) % photos.length;
-  openLightbox();
-};
+  prevBtn.onclick = e => {
+    e.stopPropagation();
+    currentIndex = (currentIndex - 1 + photos.length) % photos.length;
+    openLightbox();
+  };
 
-nextBtn.onclick = e => {
-  e.stopPropagation();
-  currentIndex = (currentIndex + 1) % photos.length;
-  openLightbox();
-};
+  nextBtn.onclick = e => {
+    e.stopPropagation();
+    currentIndex = (currentIndex + 1) % photos.length;
+    openLightbox();
+  };
 
-lightbox.onclick = () => {
-  lightbox.style.display = "none";
-};
+  lightbox.onclick = () => {
+    lightbox.style.display = "none";
+  };
+
+});
